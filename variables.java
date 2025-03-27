20e334c47ca40bece8d5dec8d399e62b02d78ef5
Dharshatashri <dharshatashri1109@gmail.com>
2025-03-27 16:09:58 +0530

    Add files via upload
    The program is a basic of how the variable acts as a container for storing the data In this program it is remarked that if you give a variable name into a double quotes or single quote thinking it a string then the variable name is not meant to be a variable. variables name should be without quotes .

---
+++ variables.java
@@ -0,0 +1,9 @@
+class Hello1{
+    public static void main(String []args){
+        int container=67;
+        String a="hi";
+        String container1 =a;
+        System.out.println(container);
+        System.out.println(container1);
+    }
+}
